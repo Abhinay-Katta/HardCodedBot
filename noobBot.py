aca56ddc5e137fcc99ee472682f6e61ae435cbe6
Abhi Katta <Abhinay Katta>
2023-03-16 18:01:59 +0530

    NoneType error fix

--- noobBot.py
+++ noobBot.py
@@ -61,37 +61,41 @@ wish_me()
 if __name__ == '__main__':
     speak("Tell me, How can I help you... ?")
     while True:
-        statement = take_command().lower()
-        if statement == 0:
-            continue
-        if "bye" in statement or "stop" in statement:
-            speak('ok your personal assistant is shutting down')
-            print('ok your personal assistant is shutting down')
-            break
-        elif 'open youtube' in statement or 'youtube' in statement:
-            webbrowser.open_new_tab("https://www.youtube.com/")
-            speak("opening youtube")
-            time.sleep(3)
-        elif 'open gmail' in statement or 'gmail' in statement:
-            webbrowser.open_new_tab("https://mail.google.com/")
-            speak("opening g mail")
-            time.sleep(3)
-        elif 'open kaggle' in statement or 'kaggle' in statement:
-            webbrowser.open_new_tab("https://www.kaggle.com/")
-            speak("opening kaggle")
-        elif 'time' in statement or 'whats the time?' in statement:
-            strTime = datetime.datetime.now().strftime("%H:%M")
-            speak(f"the time is {strTime}")
-        elif 'who are you' in statement or 'what can you do' in statement:
-            speak("I am a noob A I . I am programmed to perform simplest shitty tasks such as opening youtube or opening g mail and telling you the time. Thats all i can do for now, but ill be upgraded later in the future, which is pretty doubtfull, to be honest.")
-            speak("I was built by a dude named Abhi, further details about my guy here are confidential for no reason ")
-        elif 'log off' in statement or 'sign out' in statement:
-            speak("Ok signing off your pc, Have a good day....")
-            subprocess.call(["shutdown", "/l"])
-            break
-        elif 'open spotify' in statement or 'spotify' in statement:
-            webbrowser.open_new_tab("https://open.spotify.com/")
-            speak("opening spotify in browser")
-        speak("you want anything else...?")
-
+        try:
+            statement = take_command()
+            if statement == 0:
+                statement = input("What do you want?:\n")
+                continue
+            if "bye" in statement or "stop" in statement:
+                speak('ok your personal assistant is shutting down')
+                print('ok your personal assistant is shutting down')
+                break
+            elif 'open youtube' in statement or 'youtube' in statement:
+                webbrowser.open_new_tab("https://www.youtube.com/")
+                speak("opening youtube")
+                time.sleep(3)
+            elif 'open gmail' in statement or 'gmail' in statement:
+                webbrowser.open_new_tab("https://mail.google.com/")
+                speak("opening g mail")
+                time.sleep(3)
+            elif 'open kaggle' in statement or 'kaggle' in statement:
+                webbrowser.open_new_tab("https://www.kaggle.com/")
+                speak("opening kaggle")
+            elif 'time' in statement or 'whats the time?' in statement:
+                strTime = datetime.datetime.now().strftime("%H:%M")
+                speak(f"the time is {strTime}")
+            elif 'who are you' in statement or 'what can you do' in statement:
+                speak("I am a noob A I . I am programmed to perform simplest shitty tasks such as opening youtube or opening g mail and telling you the time. Thats all i can do for now, but ill be upgraded later in the future, which is pretty doubtfull, to be honest.")
+                speak(
+                    "I was built by a dude named Abhi, further details about my guy here are confidential for no reason ")
+            elif 'log off' in statement or 'sign out' in statement:
+                speak("Ok signing off your pc, Have a good day....")
+                subprocess.call(["shutdown", "/l"])
+                break
+            elif 'open spotify' in statement or 'spotify' in statement:
+                webbrowser.open_new_tab("https://open.spotify.com/")
+                speak("opening spotify in browser")
+            speak("you want anything else...?")
+        except:
+            statement = take_command()
 time.sleep(3)
