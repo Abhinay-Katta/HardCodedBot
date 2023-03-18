0a35be425e9f3455c2fe26c4ab6b679e32f1eff9
Abhi Katta <Abhinay Katta>
2023-03-18 23:35:15 +0530

    be-

--- app/chadBot.py
+++ app/chadBot.py
@@ -4,6 +4,7 @@ import webbrowser
 import time
 import datetime
 import subprocess
+import sys
 
 
 class chad_noob_bot:
@@ -13,6 +14,11 @@ class chad_noob_bot:
         self.engine.setProperty('voice', self.voices[0].id)
         self.commands = {
             "time": self.__get_time(),
+            "youtube": self.__open_youtube(),
+            "gmail": self.__open_gmail(),
+            "shut down": self.__shutting_off_with_outro(),
+            "who are you": self.__self_describe(),
+
         }
 
     def speak(self, text) -> None:
@@ -35,6 +41,7 @@ class chad_noob_bot:
         r = sr.Recognizer()
         with sr.Microphone() as source:
             # add listening gif here
+
             print("Listening...")
             audio = r.listen(source)
         try:
@@ -53,7 +60,28 @@ class chad_noob_bot:
         for i in range(5):
             print(f"Destroying SYSTEM32 in {6-i} seconds..\n")
             time.sleep(1)
+        self.speak('ok your personal assistant is shutting down')
+        self.text = 'ok your personal assistant is shutting down'
 
+        subprocess.call(["shutdown", "\l"])
+
+    def __open_youtube(self):
+        webbrowser.open_new_tab("https://www.youtube.com/")
+        self.speak("opening youtube")
+        time.sleep(3)
+
+    def __open_gmail(self):
+        webbrowser.open_new_tab("https://mail.google.com/")
+        self.speak("opening g mail")
+        time.sleep(3)
+
+    def __self_describe(self):
+        self.speak(
+            '''I am a noob A I . I am programmed to perform simplest shitty tasks such
+                   as opening youtube or opening g mail and telling you the time. Thats all i can do for now,
+                   but ill be upgraded later in the future, which is pretty doubtfull, to be honest.
+                   I was built by a dude named Abhi, further details about my guy here are confidential for no reason.'''
+        )
     # def run(self):
 
     #     ins = {
